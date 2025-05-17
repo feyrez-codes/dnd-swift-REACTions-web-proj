7c2116f43e58ad9c44b5ca78783038749926d888
Walwil8809 <colemanwallace8382@gmail.com>
2025-05-17 16:09:19 -0400

    v1.2 reformat template for custom code

--- src/scripts/main.tsx
+++ src/scripts/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import '../stylesheets/index.css'
+import App from './App-TSXtemplate-retired.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
